b7d5f96fec4be848c66a4365db7a38753bf40696
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-09 11:07:12 -0500

    Configured environment

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  apiGatewayUrl: 'http://localhost:8080',
+  authServiceUrl: 'http://localhost:8081',
+  paymentServiceUrl: 'http://localhost:8082',
+  productServiceUrl: 'http://localhost:8083',
+  cartServiceUrl: 'http://localhost:8084',
+  orderServiceUrl: 'http://localhost:8085',
+  userServiceUrl: 'http://localhost:8086',
+  notificationServiceUrl: 'http://localhost:8087',
+  catalogServiceUrl: 'http://localhost:8088',
+  commonServiceUrl: 'http://localhost:8089',
+  contactServiceUrl: 'http://localhost:8090'
+};
